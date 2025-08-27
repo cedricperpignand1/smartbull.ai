24ac5ad08043ce5dac9d335f46016e7aed0ea63e
cedricperpignand1 <cedricperpignand@gmail.com>
2025-08-27 15:10:54 -0400

    tried to fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "geist/font";
 import "./globals.css";
 import SessionProviderWrapper from "../providers/SessionProviderWrapper";
 import AuthGuard from "../components/AuthGuard";
