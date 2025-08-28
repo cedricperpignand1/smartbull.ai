51b85285e0bccd49b3d22f98feb4c506cc35edf7
cedricperpignand1 <cedricperpignand@gmail.com>
2025-08-27 22:06:41 -0400

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-// app/layout.tsx
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import SessionProviderWrapper from "../providers/SessionProviderWrapper";
 import AuthGuard from "../components/AuthGuard";
@@ -18,8 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* NOTE: fonts are objects, not functions */}
-      <body className={`${GeistSans.className} ${GeistMono.variable} antialiased`}>
+      <body className="font-sans antialiased">
         <SessionProviderWrapper>
           <AuthGuard>{children}</AuthGuard>
         </SessionProviderWrapper>
